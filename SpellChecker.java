b61b32f406b32fcf14e7b1c1c04c3ae8682e068e
Gijs van Horn <gijsvanhorn@gmail.com>
2015-02-11 09:36:04 +0100

    Mijn ouwe shit eruit

--- SpellChecker.java
+++ SpellChecker.java
@@ -6,8 +6,8 @@ class SpellChecker {
         int count = 0, typo = 0;
         long start = 0, end = 0;
         String wordfile, textfile;
-        //Hashtable<String, String> table;
-        CollisionChainTable table;
+        Hashtable<String, String> table;
+        //CollisionChainTable table;
 
         /* Shared token to store for every word in the hash table. */
         String placeholder = "a";
@@ -20,10 +20,10 @@ class SpellChecker {
         textfile = args[1];
         hash_size = Integer.parseInt(args[2]);
         System.out.printf("Selected table size: %d\n", hash_size);
-        //table = new Hashtable<String, String>(hash_size);
+        table = new Hashtable<String, String>(hash_size);
         
-        Compressable function = new Division(hash_size);
-        table = new CollisionChainTable(hash_size, function);
+        //Compressable function = new Division(hash_size);
+        //table = new CollisionChainTable(hash_size, function);
         
         /* Read wordfile, and insert every word into the hash table. */
         try {
@@ -54,7 +54,7 @@ class SpellChecker {
                 while(st.hasMoreTokens()) {
                     String word = st.nextToken();
                     if (!contains_numbers(word) && table.get(word) == null) {
-                        System.out.printf("Not found: [%s]\n", word);
+                        //System.out.printf("Not found: [%s]\n", word);
                         typo++;
                     }
                     count++;
