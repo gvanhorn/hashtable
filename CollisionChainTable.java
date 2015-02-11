140f3a93acac89e40187042119aa2d049f19f997
Gijs van Horn <gijsvanhorn@gmail.com>
2015-02-11 11:42:51 +0100

    collison chaining af

--- CollisionChainTable.java
+++ CollisionChainTable.java
@@ -1,29 +1,83 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 
 public class CollisionChainTable {
 	int size;
-	String[] table;
+	int wordCount;
+	ArrayList<LinkedList<Entry>> table;
 	Compressable function;
 	
 	CollisionChainTable(int hashSize, Compressable hashFunction){
 		size = hashSize;
 		function = hashFunction;
-		table = new String[hashSize];
+		wordCount = 0;
+		table = new ArrayList<LinkedList<Entry>>();
+		
+		for(int i=0; i<hashSize; i++){
+			LinkedList<Entry> emptylist = new LinkedList<Entry>();
+			table.add(emptylist);
+		}
+		
+		//System.out.println(table.size());
 		
 		
 	}
 	
+	@SuppressWarnings("unchecked")
 	public void put(String key, String value){
 		int index = function.calcIndex(key);
-		table[index]= key;
+		Entry entry = new Entry(key,value);
+		
+		table.get(index).add(entry);
+		
+		wordCount++;
+		
 		
 	}
 	
 	public String get(String key){
-		return key;
+		//hash key to find index of linkedlist and find it
+		int index = function.calcIndex(key);
+		LinkedList<Entry> tempList = table.get(index);
+		int linkedListSize = tempList.size();
+		
+		//default return value is null
+		String value = null;
+		
+		
+		//loop over linkedlist and find the key
+		for(int i = 0; i < linkedListSize; i++){
+			if(key.equals(tempList.get(i).getKey())){
+				value = tempList.get(i).getValue();	
+			}
+		}
+		
+		//System.out.println(key +" "+ value);
+		return value;
 	}
 	
 	public int size(){
 		return size;
+	}
+	
+	class Entry{
+		String key;
+		String value;
+		
+		Entry(String inputkey, String inputvalue){
+			key = inputkey;
+			value = inputvalue;
+		}
+		
+		public String getKey(){
+			return key;
+		}
+		
+		public String getValue(){
+			return value;
+		}
+		
 	}
 	
 }
--- SpellChecker.java
+++ SpellChecker.java
@@ -6,8 +6,8 @@ class SpellChecker {
         int count = 0, typo = 0;
         long start = 0, end = 0;
         String wordfile, textfile;
-        Hashtable<String, String> table;
-        //CollisionChainTable table;
+        //Hashtable<String, String> table;
+        CollisionChainTable table;
 
         /* Shared token to store for every word in the hash table. */
         String placeholder = "a";
@@ -20,10 +20,10 @@ class SpellChecker {
         textfile = args[1];
         hash_size = Integer.parseInt(args[2]);
         System.out.printf("Selected table size: %d\n", hash_size);
-        table = new Hashtable<String, String>(hash_size);
+        //table = new Hashtable<String, String>(hash_size);
         
-        //Compressable function = new Division(hash_size);
-        //table = new CollisionChainTable(hash_size, function);
+        Compressable function = new Division(hash_size);
+        table = new CollisionChainTable(hash_size, function);
         
         /* Read wordfile, and insert every word into the hash table. */
         try {
@@ -34,7 +34,7 @@ class SpellChecker {
                 copy = str.toLowerCase();
                 table.put(copy, placeholder);
             }
-            System.out.println(table.toString());
+            //System.out.println(table.toString());
             end = System.currentTimeMillis();
             in.close();
         } catch (IOException e) {
@@ -67,7 +67,7 @@ class SpellChecker {
             e.printStackTrace();
         }
 
-        System.out.printf("Hash table contains %d words\n", table.size());
+        System.out.printf("Hash table contains %d words\n", table.wordCount);
         System.out.printf("Hash table load factor %f\n",
                (double)table.size()/hash_size);
 
